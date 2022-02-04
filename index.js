42a65ab580e0f975fef43799d76176117a58818d
Bevin Duncan <bevin.duncan@gmail.com>
2022-02-04 15:00:24 -0800

    commit tree 13546d9cece9c75a07988287390a4f0e4c4696b0

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const element = document.getElementById("main");
-element.remove();
+const element = getElementById("main");
+element.remove()
 
-const newHeader = document.createElement('h1');
-newHeader.id = "victory";
 
-newHeader.textContent = "Bevin is the champion"
-document.body.append(newHeader);
+
